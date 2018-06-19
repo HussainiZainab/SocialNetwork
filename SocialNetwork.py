79e05a279a34448ad68c0e8740f4b948504f3daa
HussainiZainab <40184518+HussainiZainab@users.noreply.github.com>
2018-06-19 14:36:49 -0700

    Add files via upload

--- SocialNetwork.py
+++ SocialNetwork.py
@@ -1,57 +1,41 @@
-## Made by: Zainab Hussaini
-## My Social Network
-
-
-class User:
-    def __init__(self, firstName, lastName, username, bio, userID):
-        self.firstName = firstName
-        self.lastName = lastName
-        self.username = username
-        self.bio = bio
-        self.userID = userID
-        self.friends = []
-        self.posts = []
-        
-
-    def addFriend (self,username):
-        self.friends.append(username)
-
-##    def unFriend():
-##
-##
-    def viewNewsFeed (self, friends):
-     for Friend in self.friends:
-       print (friends.posts)
-
-
-
-
-
-if __name__== "__main__":
-    firstName = "Zainab"
-    lastName = "Hussaini"
-    username = "ZainabHussaini"
-    bio = "Welcome to my timeline."
-    userID = 0000
-
-    Zainab = User(firstName, lastName, username, bio, userID)
-    lucy = User("Lucy", "Lucille", "Lucygoosy", "I like geese", "0123")
-    print (Zainab.firstName)
-    print (lucy.firstName)
-
-    Zainab.addFriend("lucygoosy")
-    print(Zainab.friends)
-    lucy.posts.append("This Is My Friend")
-    print(lucy.posts)
-
-    Zainab = User(firstName, lastName, username, bio, userID)
-    Sara = User("Sara", "Brown", "SaraBrown", "Welcome", "2222")
-    print (Zainab.firstName)
-    print (Sara.firstName)
-    
-    Zainab.addFriend("SaraBrown")
-    print(Zainab.friends)
-    Sara.posts.append("Another Friend")
-    print(Sara.posts)
-
-Zainab.viewNewsFeed(username)
+Python 3.6.5 (v3.6.5:f59c0932b4, Mar 28 2018, 16:07:46) [MSC v.1900 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+============= RESTART: C:\Users\4170542\Desktop\SocialNetwork.py =============
+['This Is My Friend']
+['Another Friend']
+Lucygoosy
+SaraBrown
+>>> 
+============= RESTART: C:\Users\4170542\Desktop\SocialNetwork.py =============
+This is my NewsFeed
+['This Is My Friend']
+['Another Friend']
+This is my friendslist
+Lucygoosy
+SaraBrown
+Traceback (most recent call last):
+  File "C:\Users\4170542\Desktop\SocialNetwork.py", line 79, in <module>
+    Zainab.unfriend(Sara)
+  File "C:\Users\4170542\Desktop\SocialNetwork.py", line 41, in unfriend
+    for friend in friendsList:
+NameError: name 'friendsList' is not defined
+>>> 
+============= RESTART: C:\Users\4170542\Desktop\SocialNetwork.py =============
+This is my NewsFeed
+['This Is My Friend']
+['Another Friend']
+This is my friendslist
+Lucygoosy
+SaraBrown
+>>> 
+============= RESTART: C:\Users\4170542\Desktop\SocialNetwork.py =============
+This is my NewsFeed
+['This Is My Friend']
+['Another Friend']
+This is my friendslist
+Lucygoosy
+SaraBrown
+This is my *UPDATED* friendslist
+Lucygoosy
+>>> 
