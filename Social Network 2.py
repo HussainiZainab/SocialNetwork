59a0766f06523627a7380cb962d49f0622c3c9dc
HussainiZainab <40184518+HussainiZainab@users.noreply.github.com>
2018-06-20 14:52:24 -0700

    Add files via upload

--- Social Network 2.py
+++ Social Network 2.py
@@ -12,7 +12,7 @@ class User:
         self.friends.append(person)
 
     def removeFriend(self, person):
-                self.friends.remove(person)
+        self.friends.remove(person)
         
     def addFirstName(self, firstName):
         self.firstName = firstName
@@ -45,6 +45,8 @@ class User:
 
     def addComment(self, comment):
         self.comments.append(comment)
+
+        
     
 class Post:
     def __init__(self, content):
@@ -57,37 +59,42 @@ class Post:
 
 class Network:
     def __init__(self):
-       self.users = []
+        self.users = []
 
     def createUser(self, username):
         myUser = User(username)
         self.users.append(myUser)
-        myUser.createUserID(len(users))
+        myUser.createUserID(len(self.users))
 
         print ("*Usercreated")
 
 if __name__ == "__main__":
 
-    username = "ZainabHussaini"
+####    username = "ZainabHussaini"
+####
+####    Zainab = User("ZainabHussaini")
+####    Lucy = User("Lucygoosy")
+####    Sara = User("SaraBrown")
+####
+####    Zainab.addFriend(Lucy)
+####    Zainab.addFriend(Sara)
+####
+####    Lucy.posts.append("This is my friend")
+####    Sara.posts.append("Another friend")
+####
+####    Zainab.viewNewsFeed()
+####    Zainab.showUsernames()
+####    Zainab.addPost("Hello")
+####    
+####    Zainab.removeFriend(Sara)
+####    Zainab.showUsernames()
+####   
+####    Zainab.addFirstName("Zainab")
+####    Zainab.addLastName("Husssaini")
+####    Zainab.addBio("Welcome")
 
-    Zainab = User("ZainabHussaini")
-    Lucy = User("Lucygoosy")
-    Sara = User("SaraBrown")
-
-    Zainab.addFriend(Lucy)
-    Zainab.addFriend(Sara)
-
-    Lucy.posts.append("This is my friend")
-    Sara.posts.append("Another friend")
-
-    Zainab.viewNewsFeed()
-    Zainab.showUsernames()
-    Zainab.addPost("Hello")
+    network = Network()
+    network.createUser("ZainabHussaini")
     
-    Zainab.removeFriend(Sara)
-    Zainab.showUsernames()
-   
-    Zainab.addFirstName("Zainab")
-    Zainab.addLastName("Husssaini")
-    Zainab.addBio("Welcome")
+
 
